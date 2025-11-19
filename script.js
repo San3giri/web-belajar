19a5878cdad05b457de1aab7526e0a253e2a155f
San3giri <ulum.150r@gmail.com>
2025-11-19 22:51:45 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// === MENU TOGGLE ===
+const toggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+toggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+
+// === ANIMASI SCROLL ===
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+  const reveals = document.querySelectorAll('.reveal');
+
+  for (let i = 0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight;
+    const revealTop = reveals[i].getBoundingClientRect().top;
+    const revealPoint = 100;
+
+    if (revealTop < windowHeight - revealPoint) {
+      reveals[i].classList.add('active');
+    } else {
+      reveals[i].classList.remove('active');
+    }
+  }
+}
